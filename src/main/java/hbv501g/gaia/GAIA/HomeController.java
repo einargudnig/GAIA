b75c996c4f6b8ed2dd769505a006c961d2419dcd
einargudnig <einargudnig@gmail.com>
2019-11-13 08:32:43 +0000

    starting over

---
+++ src/main/java/hbv501g/gaia/GAIA/HomeController.java
@@ -0,0 +1,19 @@
+package hbv501g.gaia.GAIA;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String Home() {
+        return "Velkominn";
+    }
+
+    @RequestMapping("login")
+    public String LoginPage() {
+        return "LoginPage";
+    }
+
+}
---
+++ src/test/java/hbv501g/gaia/GAIA/GaiaApplicationTests.java
@@ -0,0 +1,13 @@
+package hbv501g.gaia.GAIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
