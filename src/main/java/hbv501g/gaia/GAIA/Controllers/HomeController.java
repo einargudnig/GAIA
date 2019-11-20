b50b9be6d13831fa1aaefa1198465ed29f363ff7
einargudnig <einargudnig@gmail.com>
2019-11-20 10:35:07 +0000

    userController

--- src/main/java/hbv501g/gaia/GAIA/Controllers/HomeController.java
+++ src/main/java/hbv501g/gaia/GAIA/Controllers/HomeController.java
@@ -1,30 +1,23 @@
 package hbv501g.gaia.GAIA.Controllers;
 
-import hbv501g.gaia.GAIA.Entities.User;
-import hbv501g.gaia.GAIA.Entities.Challenge;
+
 import hbv501g.gaia.GAIA.Services.ChallengeLogService;
 import hbv501g.gaia.GAIA.Services.ChallengeService;
-import hbv501g.gaia.GAIA.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 @Controller
 public class HomeController {
 
-    private UserService userService;
     private ChallengeService challengeService;
     private ChallengeLogService challengeLogService;
 
     @Autowired
-    public HomeController(UserService userService, ChallengeService challengeService, ChallengeLogService challengeLogService) {
-        this.userService = userService;
+    public HomeController( ChallengeService challengeService, ChallengeLogService challengeLogService) {
         this.challengeService = challengeService;
         this.challengeLogService = challengeLogService;
     }
@@ -34,53 +27,15 @@ public class HomeController {
         return "Index";
     }
 
-    @RequestMapping("login")
-    public String LoginPage() {
-        return "login";
-    }
 
     /*
     @RequestMapping("challenges")
     public String ChallengePage() { return "ChallengePage"; }
     */
 
-    /*
-    @RequestMapping("signup")
-    public String SignUp(User user) { return "SignUp"; }
-     */
-
     @RequestMapping("skilmalar")
     public String Skilmalar() { return "Skilmalar"; }
 
-    /* ******************************************************** */
-    /* SignUp for Users */
-    @RequestMapping(value = "/signup", method = RequestMethod.GET)
-    public String signUpGET(User user){
-        return "SignUp";
-    }
-
-    @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signUpPOST(@Valid User user, BindingResult result, Model model){
-        if(result.hasErrors()){
-            return "SignUp";
-        }
-        User exists = userService.findByUserName(user.userName);
-        if(exists == null){
-            userService.save(user);
-        }
-        model.addAttribute("users", userService.findAll());
-        return "/users";
-    }
-
-
-    /* ***************************************************** */
-    /* To see what users are in the database */
-    /* Virkar ad skoda /users */
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public String usersGET(Model model) {
-        model.addAttribute("users", userService.findAll());
-        return "users";
-    }
 
     /* ******************************************************** */
     /* To see challenges in the database */
@@ -91,40 +46,5 @@ public class HomeController {
         return "challenges";
     }
 
-    /* ******************************************************** */
-    /* To Log in user */
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginGET(User user){
-        return "login";
-    }
 
-    /* ATHATH Þurfum að breyta public breytum í private entities. Fyrir ALLT!
-    * Breytum svo hér fyrir neðan..
-    * User exists = userService.findUserByEmail(user.getEmail()) */
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String loginPOST(@Valid User user, BindingResult result, Model model, HttpSession session){
-        if(result.hasErrors()){
-            return "login";
-        }
-        model.addAttribute("challenges", challengeService.findAll());
-        User exists = userService.login(user);
-        if(exists != null){
-            session.setAttribute("LoggedInUser", user);
-            return "redirect:/";
-        }
-        return "redirect:/";
-    }
-
-    /* ******************************************************** */
-    /* To see logged in user */
-    @RequestMapping(value = "/loggedin", method = RequestMethod.GET)
-    public String loggedinGET(HttpSession session, Model model){
-        model.addAttribute("challenges", challengeService.findAll());
-        User sessionUser = (User) session.getAttribute("LoggedInUser");
-        if(sessionUser  != null){
-            model.addAttribute("loggedinuser", sessionUser);
-            return "loggedInUser";
-        }
-        return "redirect:/";
-    }
 }
