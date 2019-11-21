e9da0b3b849a6a4f727ba084f00dbdaae1ff2810
Viktorthor <viktorthorr@gmail.com>
2019-11-21 13:35:18 +0000

    Snyrta User.java kóða

--- src/main/java/hbv501g/gaia/GAIA/Entities/User.java
+++ src/main/java/hbv501g/gaia/GAIA/Entities/User.java
@@ -9,25 +9,25 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-   // @Column(name="id")
+    @Column(name="id")
     private long id;
 
-   // @Column(name="username")
+    @Column(name="username")
     public String userName;
 
-   // @Column(name="email")
+    @Column(name="email")
     public String email;
 
-   // @Column(name="password")
+    @Column(name="password")
     public String password;
 
-   // @Column(name="userinfo")
+    @Column(name="userinfo")
     public String userInfo;
 
-   // @Column(name="originalindex")
+    @Column(name="originalindex")
     public Double originalIndex;
 
-   // @Column(name="currindex")
+    @Column(name="currindex")
     public Double currIndex;
 
 
