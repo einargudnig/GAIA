8266cd99c71f3729eb8102c57d88e4eaf3ba7dc3
Viktorthor <viktorthorr@gmail.com>
2019-11-25 13:03:31 +0000

    addChallenge virkni hálfkláruð, eftir að geta skráð í db

--- src/main/java/hbv501g/gaia/GAIA/Services/UserService.java
+++ src/main/java/hbv501g/gaia/GAIA/Services/UserService.java
@@ -1,5 +1,6 @@
 package hbv501g.gaia.GAIA.Services;
 
+import hbv501g.gaia.GAIA.Entities.Challenge;
 import hbv501g.gaia.GAIA.Entities.User;
 
 import java.util.List;
@@ -14,5 +15,5 @@ public interface UserService {
     User findByUserName(String userName);
     User findByEmail(String email);
     User login(User user);
-
+    User addChallengeToUser(User user, Challenge challenge);
 }
